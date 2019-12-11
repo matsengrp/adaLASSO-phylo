09c43a20b1de09390b49b72fa6ad80ccb06522ef
Erick Matsen <ematsen@gmail.com>
2019-12-11 07:05:59 -0800

    adding local raxml-ng for linux

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,9 @@ FROM matsen/phyloinfer
 COPY . /adalasso
 WORKDIR /adalasso
 
-RUN /opt/conda/bin/conda env update -n phyloinfer -f environment.yml
+RUN ln -s /adalasso/bin/linux/raxml-ng /usr/bin
 
 RUN /opt/conda/bin/conda run -n phyloinfer jupyter nbconvert --to notebook --inplace --execute --ExecutePreprocessor.timeout=10000 examples/denv4-brazil.ipynb
 RUN /opt/conda/bin/conda run -n phyloinfer jupyter nbconvert --to notebook --inplace --execute --ExecutePreprocessor.timeout=10000 examples/FISTA-vs-ISTA.ipynb
 RUN /opt/conda/bin/conda run -n phyloinfer jupyter nbconvert --to notebook --inplace --execute --ExecutePreprocessor.timeout=10000 examples/short-edge-support.ipynb
 RUN /opt/conda/bin/conda run -n phyloinfer jupyter nbconvert --to notebook --inplace --execute --ExecutePreprocessor.timeout=10000 examples/simulated-100tips.ipynb
-
-
--- optimizer.py
+++ optimizer.py
@@ -17,16 +17,16 @@ def raxml(sequences, starting_tree="pars", model="JC", bootstrap=False, fixed_tr
     raxml_exe = None
     try:
         from Bio._py3k import getoutput
-        output = getoutput("./raxml-ng -v")
+        output = getoutput("raxml-ng -v")
         if "not found" not in output and "RAxML-NG" in output:
-            raxml_exe = "./raxml-ng"
+            raxml_exe = "raxml-ng"
     except OSError:
         pass
 
     if not raxml_exe:
         raise MissingExternalDependencyError(
             "Install RAxML (binary raxml-ng) if you want to test the Bio.Phylo.Applications wrapper.")
-    
+
     if not fixed_tree:
         if bootstrap:
             cmd = RaxmlCommandline(raxml_exe, bootstrap=None, sequences=sequences, model=model, starting_tree=starting_tree, bstrees=bstrees)
@@ -34,10 +34,10 @@ def raxml(sequences, starting_tree="pars", model="JC", bootstrap=False, fixed_tr
             cmd = RaxmlCommandline(raxml_exe, sequences=sequences, model=model, starting_tree=starting_tree)
     else:
         cmd = RaxmlCommandline(raxml_exe, fixtree=None, sequences=sequences, model=model, starting_tree=starting_tree)
-      
+
     out, err = cmd()
     if log:
-        print out      
+        print out
 
 
 def ista(model, tree, brlen, lam, gamma, beta, wts, maxiter=MAX_ITER, abstol=ABSTOL, minstepsz=MIN_STEP_SIZE):
@@ -45,11 +45,11 @@ def ista(model, tree, brlen, lam, gamma, beta, wts, maxiter=MAX_ITER, abstol=ABS
     objval_ll = []
     objval_lasso = []
     lam_ada = lam
-    
+
     curr_ll = model.loglikelihood(tree, brlen_x)
     objval_ll.append(curr_ll)
     objval_lasso.append(curr_ll-gamma*np.linalg.norm(brlen_x*wts, 1))
-    
+
     for k in range(maxiter):
         grad_brlen_x = -model.loglikelihood(tree, brlen_x, grad=True)
         if lam_ada < minstepsz:
@@ -65,23 +65,23 @@ def ista(model, tree, brlen, lam, gamma, beta, wts, maxiter=MAX_ITER, abstol=ABS
         objval_lasso.append(curr_ll-gamma*np.linalg.norm(brlen_x*wts, 1))
         if abstol is not None and np.abs(objval_lasso[-1]-objval_lasso[-2]) < abstol:
             break
-    
-    return brlen_x, objval_ll, objval_lasso, lam_ada
-    
 
-def fista(model, tree, brlen, lam, gamma, beta, wts, prox='l1', 
+    return brlen_x, objval_ll, objval_lasso, lam_ada
+
+
+def fista(model, tree, brlen, lam, gamma, beta, wts, prox='l1',
           maxiter=MAX_ITER, abstol=ABSTOL, minstepsz=MIN_STEP_SIZE, stepsz_restart=True, monitor=False):
     brlen_x, brlen_prev_x = brlen, brlen
     objval_ll = []
     objval_penalized = []
     lam_ada = lam
-    
+
     prop_ll = model.loglikelihood(tree, brlen_x)
     objval_ll.append(prop_ll)
     objval_penalized.append(prop_ll-penalty_value(brlen_x*wts, gamma, method=prox))
-    
+
     restart = 0
-    
+
     for k in range(maxiter):
         brlen_y = np.maximum(brlen_x + ((k-restart)/(k-restart+3.0)) * (brlen_x - brlen_prev_x), 0.0)
         curr_ll = model.loglikelihood(tree, brlen_y)
@@ -90,26 +90,26 @@ def fista(model, tree, brlen, lam, gamma, beta, wts, prox='l1',
                 print "iteration {}; step size {}; {}, restarting ...".format(k, lam_ada, ['step size too small', 'current loglikelihood is infinite'][np.isinf(curr_ll)])
             restart, lam_ada = k+1, lam
             continue
-            
+
         grad_brlen_y = -model.loglikelihood(tree, brlen_y, grad=True)
         while True:
             brlen_z = np.maximum(prox_next(brlen_y-lam_ada*grad_brlen_y, lam_ada*gamma*wts, method=prox), 0.0)
-                
+
             prop_ll = model.loglikelihood(tree, brlen_z)
             if -prop_ll <= -curr_ll + np.dot(grad_brlen_y.T, brlen_z-brlen_y) + (1.0/(2*lam_ada))*np.sum((brlen_z-brlen_y)**2):
                 break
             lam_ada *= beta
-        
-        
+
+
         brlen_prev_x, brlen_x = brlen_x, brlen_z
         objval_ll.append(prop_ll)
         objval_penalized.append(prop_ll - penalty_value(brlen_x*wts, gamma, method=prox))
-        
+
         if abstol is not None and np.abs(objval_penalized[-1]-objval_penalized[-2]) < abstol:
             break
-    
+
     return brlen_x, objval_ll, objval_penalized, lam_ada, k+1
-    
+
 
 def adaLasso(model, tree, brlen, lam, gamma, beta, prox='l1', msteps=4, gamma_ada_penalized=1.0, maxiter=MAX_ITER,
              abstol=ABSTOL, minstepsz=MIN_STEP_SIZE, monitor=False, sparsity_monitor=False):
@@ -121,35 +121,35 @@ def adaLasso(model, tree, brlen, lam, gamma, beta, prox='l1', msteps=4, gamma_ad
     objval_penalized = []
     brlen_x_msteps = []
     n_zeros = np.empty(msteps)
-    init_lam = lam 
-    
+    init_lam = lam
+
 
     for m in range(msteps):
         if m>0 and prox=='l2':
             brlen_x, objval_ll_step, objval_penalized_step, lam, niter = fista(model, tree, brlen_x, lam, gamma, beta, wts, maxiter=maxiter, abstol=abstol, minstepsz=minstepsz, monitor=monitor)
         else:
             brlen_x, objval_ll_step, objval_penalized_step, lam, niter = fista(model, tree, brlen_x, lam, gamma, beta, wts, prox=prox, maxiter=maxiter, abstol=abstol, minstepsz=minstepsz, monitor=monitor)
-        
+
         n_zeros[m] = np.sum(brlen_x==0.0)
         if sparsity_monitor:
             print "cycle {}: [niter {}; # zeros: {}; step size: {}]".format(m+1, niter, n_zeros[m], lam)
-            
+
         if lam < minstepsz:
             lam = init_lam
-        
+
         wts = 1.0/(brlen_x+EPS)**gamma_ada_penalized
-        
+
         mean_wts = 1.0/np.mean(1.0/wts)
         gamma *= mean_wts_prev/mean_wts
         mean_wts_prev = mean_wts
-        
-        
+
+
         brlen_x_msteps.append(brlen_x)
         objval_ll.extend(objval_ll_step)
         objval_penalized.extend(objval_penalized_step)
-        
+
     return brlen_x_msteps, objval_ll, objval_penalized, n_zeros, lam
 
-      
 
-      
+
+
